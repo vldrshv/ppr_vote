7b2420a4c1f2db4a0b7f3be052bcc69bb16c2a1e
vershov <vldrshv@gmail.com>
2019-09-25 12:27:18 +0300

    initial

---
+++ app/src/main/java/com/example/ppr_vote/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.ppr_vote.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
